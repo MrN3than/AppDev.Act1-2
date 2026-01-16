6a3ee118a2a327c68eaa635e91c29c9920bbfdb8
MrN3than <nethanedrygragas@gmail.com>
2026-01-16 23:55:27 +0800

    Added switch case for the menu.
    This is a commit from GitHub directly and which I will try to pull to my local.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("String Manipulation Toolkit");
 Console.WriteLine("[1] String Reversal \n [2] Word Count \n [3] Character Count \n [4] Lower Case Converter [5] Upper Case Converter");
 Console.Write("Enter your choice (1-5) \n >"); 
+
+int intChoice;
+
+switch (intChoice) {
+    case 1:
+        Console.WriteLine("String Reversal");
+        break;
+    case 2:
+        Console.WriteLine("Word Count");
+        break;
+    case 3:
+        Console.WriteLine("Character Count");
+        break;
+    case 4:
+        Console.WriteLine("Lower Case Converter");
+        break;
+    case 5:
+        Console.WriteLine("Upper Case Converter");
+        break;
+    default:
+        Console.WriteLine("Invalid input. Please enter a number between 1-10.");
+        continue;
+}
