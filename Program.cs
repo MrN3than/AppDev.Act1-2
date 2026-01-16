b87c3aa1924913f72922db814c8ae117455b9323
MrN3than <nethanedrygragas@gmail.com>
2026-01-16 23:46:46 +0800

    First try of commit command. Created menu options as backbone for program.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("String Manipulation Toolkit");
+Console.WriteLine("[1] String Reversal \n [2] Word Count \n [3] Character Count \n [4] Lower Case Converter [5] Upper Case Converter");
+Console.Write("Enter your choice (1-5) \n >"); 
